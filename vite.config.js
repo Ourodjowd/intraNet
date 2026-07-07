2cfbcc03ade7109833627b881f89081534a4aafd
Ourodjowd <ourodjowd3@gmail.com>
2026-07-07 12:28:49 +0000

    feat: configure dev server port and host settings in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    host: true,
+  },
   preview: {
     port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
     host: true,
